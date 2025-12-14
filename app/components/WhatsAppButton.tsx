3617742d97e4ab250b172a46552755a16bf7568c
Suvir Jha <suvirjhamusic@gmail.com>
2025-12-14 20:48:10 +0530

    Update home page code to fix deployment issues

--- app/components/WhatsAppButton.tsx
+++ app/components/WhatsAppButton.tsx
@@ -1,7 +1,7 @@
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/919876543210"
+      href="https://wa.me/917990269668"
       target="_blank"
       rel="noopener noreferrer"
       className="
